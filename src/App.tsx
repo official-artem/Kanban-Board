4bacd05273e6ced303e244ee61d8440df501ba35
Artem Hrechuk <grechuck.artem.ag@gmail.com>
2023-03-28 12:14:16 +0300

    created Column and Task complonents

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { Heading, Container, SimpleGrid } from '@chakra-ui/react'
+import { ColumnType } from './utils/enums'
+import Column from './components/Column';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Heading
+      fontSize={{ base: '4xl', sm: '5xl', md: '6xl'}}
+      fontWeight="bold"
+      textAlign="center"
+      bgGradient="linear(to-l, #7920CA, #FF0080)"
+      bgClip="text"
+      mt={4}
+    >
+      Welcome here
+    </Heading>
+    <Container
+      maxWidth="container.lg"
+      px={4}
+      py={10}
+    >
+      <SimpleGrid
+        columns={{ base: 1, md: 4}} spacing={{ base: 16, md: 4}}>
+          <Column column={ColumnType.TO_DO} />
+          <Column column={ColumnType.IN_PROGRESS} />
+          <Column column={ColumnType.BLOCKED} />
+          <Column column={ColumnType.COMPLETED} />
+      </SimpleGrid>
+    </Container>
+    </>
   )
 }
 
